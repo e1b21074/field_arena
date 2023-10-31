184d8aaaf4084b65e00921cd2d4cc604d87b40c0
e1b21074 <e1b21074@oit.ac.jp>
2023-10-31 15:22:00 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/rumba/field_arena/FieldArenaApplication.java
@@ -0,0 +1,13 @@
+package oit.is.rumba.field_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/rumba/field_arena/FieldArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.rumba.field_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
