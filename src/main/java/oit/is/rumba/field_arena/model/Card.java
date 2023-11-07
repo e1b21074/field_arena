a587577568fc04e039f7dd606753185c9af81c01
e1b21073 <e1b21073@oit.ac.jp>
2023-11-07 15:47:38 +0900

    カード情報をrule.htmlに表示

---
+++ src/main/java/oit/is/rumba/field_arena/model/Card.java
@@ -0,0 +1,32 @@
+package oit.is.rumba.field_arena.model;
+
+public class Card {
+  int id;
+  int cardAttribute;
+  int cardStrong;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getCardAttribute() {
+    return cardAttribute;
+  }
+
+  public void setCardAttribute(int cardAttribute) {
+    this.cardAttribute = cardAttribute;
+  }
+
+  public int getCardStrong() {
+    return cardStrong;
+  }
+
+  public void setCardStrong(int cardStrong) {
+    this.cardStrong = cardStrong;
+  }
+
+}
